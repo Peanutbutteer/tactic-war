b16c9dd1457e1eda484333a2a5fbe7d69cea9aed
Satjawat Panakarn <peanutbutteer@gmail.com>
2017-02-16 23:19:01 +0700

    Initial Project

---
+++ Assets/Lobby/Scripts/Lobby/LobbyMainMenu.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace Prototype.NetworkLobby
+{
+    //Main menu, mainly only a bunch of callback called by the UI (setup throught the Inspector)
+    public class LobbyMainMenu : MonoBehaviour 
+    {
+        public LobbyManager lobbyManager;
+
+        public RectTransform lobbyServerList;
+        public RectTransform lobbyPanel;
+
+        public InputField matchNameInput;
+
+        public void OnEnable()
+        {
+            lobbyManager.topPanel.ToggleVisibility(true);
+
+            matchNameInput.onEndEdit.RemoveAllListeners();
+            matchNameInput.onEndEdit.AddListener(onEndEditGameName);
+
+        }
+
+        public void OnClickCreateMatchmakingGame()
+        {
+            lobbyManager.StartMatchMaker();
+            lobbyManager.matchMaker.CreateMatch(
+                matchNameInput.text,
+                (uint)lobbyManager.maxPlayers,
+                true,
+				"", "", "", 0, 0,
+				lobbyManager.OnMatchCreate);
+
+            lobbyManager.backDelegate = lobbyManager.StopHost;
+            lobbyManager._isMatchmaking = true;
+            lobbyManager.DisplayIsConnecting();
+
+            lobbyManager.SetServerInfo("Matchmaker Host", lobbyManager.matchHost);
+        }
+
+        void onEndEditGameName(string text)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                OnClickCreateMatchmakingGame();
+            }
+        }
+
+    }
+}
