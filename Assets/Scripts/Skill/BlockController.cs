71d3fa097b6dc62ac792cdad91abd3dd15755ba1
Knack <blacksheep_knack@hotmail.com>
2017-03-07 19:05:30 +0700

    skill , player controller

--- Assets/Scripts/Skill/BlockController.cs
+++ Assets/Scripts/Skill/BlockController.cs
@@ -3,20 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using CnControls;
 using UnityEngine.UI;
-public class BlockController : MonoBehaviour {
+using UnityEngine.Networking;
+public class BlockController : NetworkBehaviour
+{
 
     public GameObject block;
-    public GameObject player;
+    public GameObject controller;
 	public GameObject cooldownPrefab;
     private Animator anim;
-
 	private GameObject cooldownSkill;
 
 	// Use this for initialization
 	void Start () {
 		GameObject canvas = GameObject.FindGameObjectWithTag ("Canvas");
 		cooldownSkill = Instantiate (cooldownPrefab, canvas.transform, false);
-        anim = player.GetComponent<Animator>();
+        anim = GetComponent<Animator>();
 	}
 
 	void FixedUpdate() {
@@ -24,10 +25,15 @@ public class BlockController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        if (!isLocalPlayer)
+        {
+            return;
+        }
         if (CnInputManager.GetButtonUp("BlockButton"))
         {
 			cooldownSkill.SetActive (true);
-            block.SetActive(true);
+            CmdSpawnBlockSkill();
             anim.SetBool("Block", true);
             StartCoroutine(Block());
         }
@@ -39,6 +45,21 @@ public class BlockController : MonoBehaviour {
         yield return new WaitForSeconds(0.5f);
         anim.SetBool("Block", false);
         yield return new WaitForSeconds(0.5f);
-        block.SetActive(false);
     }
+
+    [Command]
+    void CmdSpawnBlockSkill()
+    {
+        var blockSkill = (GameObject)Instantiate(
+            block
+            , new Vector3(transform.position.x,transform.position.y+(70*0.03f),transform.position.z+(25*0.03f))
+            , transform.rotation);
+        blockSkill.transform.parent = transform;
+        NetworkServer.Spawn(blockSkill);
+        Destroy(blockSkill, 1f);
+    }
+
+
+    //var myNewSmoke = Instantiate(poisonSmoke, Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+    //myNewSmoke.transform.parent = gameObject.transform;
 }
