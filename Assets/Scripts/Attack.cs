c9bcee12113f6310b49c346ff5be928cfc9a343d
Satjawat Panakarn <peanutbutteer@gmail.com>
2017-03-31 22:56:27 +0700

    add attack button

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -2,48 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
-
+using CnControls;
 public class Attack : NetworkBehaviour
 {
 
-    public GameObject Staff;
-    Animator anim;
-    
-    void Start () {
-        anim = GetComponent<Animator>();
-    }
-	
-	void Update () {
+	public GameObject Staff;
+	Animator anim;
 
-        if (!isLocalPlayer)
-        {
-            return;
-        }
-        if (Input.GetKeyDown("space"))
-        {
-            Staff.GetComponent<BoxCollider>().enabled = true;
-            anim.SetBool("AttackStaff",true);
-            StartCoroutine(AttackStaff());
-        }
-    }
+	void Start()
+	{
+		anim = GetComponent<Animator>();
+	}
 
-    IEnumerator AttackStaff()
-    {
-        yield return new WaitForSeconds(0.5f);
-        anim.SetBool("AttackStaff", false);
-        Staff.GetComponent<BoxCollider>().enabled = false;
-    }
+	void Update()
+	{
 
-    public void AttackPlayer(GameObject player)
-    {
-        CmdServerTakeDamage(player,10);
-    }
+		if (!isLocalPlayer)
+		{
+			return;
+		}
+		if (CnInputManager.GetButtonDown("ExtraButton"))
+		{
+			Staff.GetComponent<BoxCollider>().enabled = true;
+			anim.SetBool("AttackStaff", true);
+			StartCoroutine(AttackStaff());
+		}
+	}
 
-    [Command]
-    public void CmdServerTakeDamage(GameObject player, int amount)
-    {
-        var health = player.GetComponent<PlayerHealth>();
-        health.TakeDamage(amount);
-        Debug.Log("CmdServerTakeDamage");
-    }
+	IEnumerator AttackStaff()
+	{
+		yield return new WaitForSeconds(0.5f);
+		anim.SetBool("AttackStaff", false);
+		Staff.GetComponent<BoxCollider>().enabled = false;
+	}
+
+	public void AttackPlayer(GameObject player)
+	{
+		CmdServerTakeDamage(player, 10);
+	}
+
+	[Command]
+	public void CmdServerTakeDamage(GameObject player, int amount)
+	{
+		var health = player.GetComponent<PlayerHealth>();
+		health.TakeDamage(amount);
+		Debug.Log("CmdServerTakeDamage");
+	}
 }
